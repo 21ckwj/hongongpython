0873b6d75d77e78511c72df72829cd6b84cebde2
21ckwj <21ckwj@naver.com>
2022-01-11 20:50:00 +0900

    update_0110

---
+++ Chapter07 모듈/Ch07-3 모듈 만들기/Circle.py
@@ -0,0 +1,14 @@
+
+from math import pi
+
+PI = pi
+
+def number_input() :
+    output = input("숫자 입력> ")
+    return float(output)
+
+def get_circumference(radius):
+    return 2 * pi * radius
+
+def get_circle_area(radius):
+    return PI * radius ** 2
